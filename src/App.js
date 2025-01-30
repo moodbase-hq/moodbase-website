f53d242531006c68810e6c8f69b69f01a0f5f008
Julia <julia@xemantic.com>
2025-01-30 18:46:59 +0100

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Website from './components/Website';
 import DatabasePage from './components/DatabasePage';
 
