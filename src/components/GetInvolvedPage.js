6505c4bf01f8a604bcfd99f34b4405c4874f807c
Julia <julia@xemantic.com>
2025-05-05 07:13:51 +0200

    fixes footer once and for all!

--- src/components/GetInvolvedPage.js
+++ src/components/GetInvolvedPage.js
@@ -8,7 +8,46 @@ import BackgroundBlob from "../components/shared/BackgroundBlob"; // Import Back
 import getInvolvedData from "../content/getinvolved.json";
 import CurvedFooter from "./shared/FooterWithCurve"; // Import the CurvedFooter
 import "./GetInvolvedPage.css";
+import Footer from "./shared/FooterWithCurve";
 
+const WaveDivider = ({ position = 'bottom', color = '#2F5EA8', className = '' }) => {
+  // Use different curves based on position
+  if (position === 'bottom') {
+    return (
+      <div className={`relative w-full overflow-hidden pointer-events-none ${className}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          preserveAspectRatio="none"
+          className="w-full h-auto block"
+          style={{ marginBottom: '-5px', display: 'block' }} // Fix for seams
+        >
+          <path
+            fill={color}
+            d="M0,96L48,106.7C96,117,192,139,288,128C384,117,480,75,576,80C672,85,768,139,864,154.7C960,171,1056,149,1152,122.7C1248,96,1344,64,1392,48L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          />
+        </svg>
+      </div>
+    );
+  } else {
+    return (
+      <div className={`relative w-full overflow-hidden pointer-events-none ${className}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          preserveAspectRatio="none"
+          className="w-full h-auto block"
+          style={{ marginTop: '-5px', display: 'block' }} // Fix for seams
+        >
+          <path
+            fill={color}
+            d="M0,320L48,304C96,288,192,256,288,245.3C384,235,480,245,576,229.3C672,213,768,171,864,165.3C960,160,1056,192,1152,186.7C1248,181,1344,139,1392,117.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          />
+        </svg>
+      </div>
+    );
+  }
+};
 
 const SupportOption = ({ item, delay = 0 }) => {
   const theme = useTheme();
@@ -340,11 +379,16 @@ const GetInvolvedPage = () => {
         </section>
 
       </Layout>
-        <CurvedFooter /> {/* Add the CurvedFooter component here */}
-        
-    </div>
-    
-            );
-};
+      {/* Transition to footer */}
+      <div className={`relative z-10 `}>
+        <WaveDivider position="bottom" color={'#2F5EA8'} />
+      </div>
 
+      {/* Footer - Keep the blue background */}
+      <section style={{ backgroundColor: '#2F5EA8' }} className="relative z-10">
+        <Footer />
+      </section>
+    </div>
+  );
+};
 export default GetInvolvedPage;
--- src/components/shared/FooterWithCurve.js
+++ src/components/shared/FooterWithCurve.js
@@ -3,6 +3,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../../context/ThemeContext';
 
+const WaveDivider = ({ position = 'bottom', color = '#2F5EA8', className = '' }) => {
+  // Use different curves based on position
+  if (position === 'bottom') {
+    return (
+      <div className={`relative w-full overflow-hidden pointer-events-none ${className}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          preserveAspectRatio="none"
+          className="w-full h-auto block"
+          style={{ marginBottom: '-5px', display: 'block' }} // Fix for seams
+        >
+          <path
+            fill={color}
+            d="M0,96L48,106.7C96,117,192,139,288,128C384,117,480,75,576,80C672,85,768,139,864,154.7C960,171,1056,149,1152,122.7C1248,96,1344,64,1392,48L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          />
+        </svg>
+      </div>
+    );
+  } else {
+    return (
+      <div className={`relative w-full overflow-hidden pointer-events-none ${className}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          preserveAspectRatio="none"
+          className="w-full h-auto block"
+          style={{ marginTop: '-5px', display: 'block' }} // Fix for seams
+        >
+          <path
+            fill={color}
+            d="M0,320L48,304C96,288,192,256,288,245.3C384,235,480,245,576,229.3C672,213,768,171,864,165.3C960,160,1056,192,1152,186.7C1248,181,1344,139,1392,117.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          />
+        </svg>
+      </div>
+    );
+  }
+};
+
 const FooterWithCurve = () => {
   const theme = useTheme();
 
